15437285df30ebfc275cdf3925c8ecdde329aab6
toriyamaiori <iori.wakuwaku.programing@gmail.com>
2026-04-15 14:49:03 +0900

    クリップボードの設定を変更してシステムクリップボードを上書きしてしまう問題を修正した

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,10 @@
 require "nvchad.autocmds"
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Sync yank to system clipboard without unnamedplus",
+  callback = function()
+    if vim.v.event.operator == "y" then
+      vim.fn.setreg("+", vim.v.event.regcontents)
+    end
+  end,
+})
--- lua/options.lua
+++ lua/options.lua
@@ -8,5 +8,8 @@ vim.opt.timeout = true
 vim.opt.timeoutlen = 250
 vim.opt.exrc = true
 vim.opt.wrap = false
-vim.o.clipboard = "unnamedplus"
+-- ヤンク時のみシステムクリップボードへ同期する（autocmds.luaで設定）
+-- ペースト時のクリップボード読み取り許可ダイアログを回避するため
+-- unnamedplusは使わない
+-- システムクリップボードからペーストしたい場合は "+p を使用
 vim.o.winborder = "rounded"
